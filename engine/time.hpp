c7cd65ede1bee69700127ab645b5d6ee8b8f521b
passingbyhelpingout1 <suterp39@gmail.com>
2025-09-08 07:06:05 +1000

    Update time.hpp

--- engine/time.hpp
+++ engine/time.hpp
@@ -1,8 +1,9 @@
 #pragma once
 #include <chrono>
+#include "mars/timing.hpp"
 
 namespace eng {
-  using clock        = std::chrono::steady_clock;              // monotonic
-  using seconds_f64  = std::chrono::duration<double>;
-  constexpr double   kFixedDt = 1.0 / 20.0;                    // 20 Hz simulation
+  using clock       = std::chrono::steady_clock;
+  using seconds_f64 = std::chrono::duration<double>;
+  // DO NOT define kFixedDt here; use mars::kFixedDt from mars/timing.hpp
 }
