45334dd695880354a1b7bc2607235df6c94fb506
passingbyhelpingout1 <suterp39@gmail.com>
2025-09-07 17:15:18 +1000

    Update Get-MarsExe.cmd

--- Get-MarsExe.cmd
+++ Get-MarsExe.cmd
@@ -1,6 +1,6 @@
 @echo off
-setlocal
-cd /d "%~dp0"
-powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0Get-MarsExe.ps1"
-echo.
-pause
+setlocal enabledelayedexpansion
+cmake -S . -B build -DCMAKE_BUILD_TYPE=Release || exit /b 1
+cmake --build build --config Release || exit /b 1
+if exist build\Release\mars_colony.exe copy /Y build\Release\mars_colony.exe . >nul
+echo Done.
