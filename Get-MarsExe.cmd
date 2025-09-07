436453d9e3fa90a4a55d7baa28ba8f1f9ff214d0
passingbyhelpingout1 <suterp39@gmail.com>
2025-09-07 17:29:42 +1000

    Update Get-MarsExe.cmd

--- Get-MarsExe.cmd
+++ Get-MarsExe.cmd
@@ -1,6 +1,11 @@
 @echo off
 setlocal enabledelayedexpansion
-cmake -S . -B build -DCMAKE_BUILD_TYPE=Release || exit /b 1
-cmake --build build --config Release || exit /b 1
+
+rem Configure with Visual Studio generator
+cmake -S . -B build -G "Visual Studio 17 2022" -A x64 || exit /b 1
+
+rem Parallel build (CMake will pass /m to MSBuild)
+cmake --build build --config Release --parallel 2 || exit /b 1
+
 if exist build\Release\mars_colony.exe copy /Y build\Release\mars_colony.exe . >nul
-echo Done.
+echo Built: build\Release\mars_colony.exe
